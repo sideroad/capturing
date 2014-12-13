6c464cda8d24bc515f8b74f919843afcbce1382d
sideroad <sideroad.jp@gmail.com>
2014-12-13 23:36:42 +0900

    cap

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var phantom = require('phantom');
 
-var grabzit = require('grabzit');
-var client = new grabzit(process.env.GRABZIT_KEY, process.env.GRABZIT_SECRET);
 
 router.get('/cap/:name/:pt/', function(req, res) {
   var name = req.params.name,
       pt = req.params.pt,
       path = "public/images/"+name+"-"+pt+".png";
-  client.set_image_options("http://capturing.herokuapp.com/create/"+encodeURIComponent(name)+"/"+pt+"/", {"format":"png"});
-  client.save_to(path);
+
+
+  phantom.create(function (ph) {
+    ph.createPage(function (page) {
+      page.open("http://capturing.herokuapp.com/create/"+encodeURIComponent(name)+"/"+pt+"/", function (status) {
+        page.evaluate(function () { return document.title; }, function (result) {
+          console.log('Page title is ' + result);
+          ph.exit();
+        });
+      });
+    });
+  });
 
   var tick = function(){
     if(fs.existsSync(path)){
@@ -27,13 +36,8 @@ router.get('/cap/:name/:pt/', function(req, res) {
 });
 
 router.get('/create/:name/:pt/', function(req, res){
-  res.send('<!DOCTYPE html><html><head></head><body><div style="font-size:'+req.params.pt+'pt;">'+req.params.name+'</div></body></html>');
+  res.send('<!DOCTYPE html><html><head><title>abc</title></head><body><div style="font-size:'+req.params.pt+'pt;">'+req.params.name+'</div></body></html>');
   res.end();
-});
-
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
