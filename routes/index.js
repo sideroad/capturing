7dde9e6bf49377a45e1bbf307559c46671e9ab66
sideroad <sideroad.jp@gmail.com>
2014-12-14 00:58:37 +0900

    capture

--- routes/index.js
+++ routes/index.js
@@ -2,41 +2,57 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var phantom = require('phantom');
+var _ = require('lodash');
+var querystring = require('querystring');
 
-
-router.get('/cap/:name/:pt/', function(req, res) {
+router.get('/cap/:name/:width/:height/', function(req, res) {
   var name = req.params.name,
-      pt = req.params.pt,
-      path = "public/images/"+name+"-"+pt+".png";
+      width = req.params.width,
+      height = req.params.height,
+      path = "public/images/"+name+"-"+width+"-"+height+".png";
 
 
   phantom.create(function (ph) {
     ph.createPage(function (page) {
-      page.open("http://capturing.herokuapp.com/create/"+encodeURIComponent(name)+"/"+pt+"/", function (status) {
-        page.evaluate(function () { return document.title; }, function (result) {
-          console.log('Page title is ' + result);
-          ph.exit();
+      page.open("http://localhost:3000/create/"+encodeURIComponent(name)+"/"+width+"/"+height+"?"+querystring.stringify(req.query), function (status) {
+      // page.open("http://capturing.herokuapp.com/create/"+encodeURIComponent(name)+"/?"+req.query, function (status) {
+        page.set('viewportSize', {
+          width: width,
+          height: height
+        });
+        page.render(path, function(){
+          res.sendFile(path, {
+            root: __dirname + '/../',
+          });
+          // res.end();
         });
       });
     });
   });
 
-  var tick = function(){
-    if(fs.existsSync(path)){
-      res.send(path);
-      res.end();
-    } else {
-      setTimeout(function(){
-        tick();
-      }, 200);
-    }
-  };
-  tick();
-
 });
 
-router.get('/create/:name/:pt/', function(req, res){
-  res.send('<!DOCTYPE html><html><head><title>abc</title></head><body><div style="font-size:'+req.params.pt+'pt;">'+req.params.name+'</div></body></html>');
+router.get('/create/:name/:width/:height/', function(req, res){
+
+  var style = _.chain({
+                'text-overflow': 'ellipsis',
+                'white-space': 'nowrap',
+                'overflow': 'hidden',
+                'font-family': 'Hiragino Kaku Gothic Pro'
+               })
+               .extend(req.params, {
+                name: '',
+                width: '100%'
+               })
+               .map(function(val, key){
+                return val ? key+': '+val : '';
+               })
+               .compact()
+               .value()
+               .join('; ')+';';
+
+  console.log(style);
+  res.send('<!DOCTYPE html><html><head><title>abc</title><link rel="stylesheet" type="text/css" href="/stylesheets/style.css"></head><body><div style="'+style+'">'+req.params.name+'</div></body></html>');
   res.end();
 });
 
