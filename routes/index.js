1862d63873cc276f5fd0f7c19d78b4d7d5f3ebf5
sideroad <sideroad.jp@gmail.com>
2014-12-13 22:10:26 +0900

    first

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+
+var grabzit = require('grabzit');
+var client = new grabzit(process.env.GRABZIT_KEY, process.env.GRABZIT_SECRET);
+
+router.get('/cap/:name/:pt/', function(req, res) {
+  var name = req.params.name,
+      pt = req.params.pt,
+      path = "public/images/"+name+"-"+pt+".png";
+  client.set_image_options("http://capturing.herokuapp.com/create/"+encodeURIComponent(name)+"/"+pt+"/", {"format":"png"});
+  client.save_to(path);
+
+  var tick = function(){
+    if(fs.exist(path)){
+      res.send(path);
+      res.end();
+    } else {
+      setTimeout(function(){
+        tick();
+      }, 200);
+    }
+  };
+  tick();
+
+});
+
+router.get('/create/:name/:pt/', function(req, res){
+  res.send('<div style="font-size:'+req.params.pt+'pt;">'+req.params.name+'</div>');
+  res.end();
+});
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
